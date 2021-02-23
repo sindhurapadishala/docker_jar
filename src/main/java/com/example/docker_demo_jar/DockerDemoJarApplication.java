64221167c351aea7828c43faa89b1d99aa7c9d44
Sindhu <psindhura1496@gmail.com>
2021-02-23 11:14:39 +0530

    adding the project

---
+++ src/main/java/com/example/docker_demo_jar/DockerDemoJarApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_demo_jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoJarApplication.class, args);
+	}
+
+}
